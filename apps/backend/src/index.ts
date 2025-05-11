b2457d2942a588c447d0467fbc428af49fdd7f4b
Mate Szedlak <szedlakmate@gmail.com>
2025-05-12 00:45:12 +0100

    Refactor: add service layer

--- apps/backend/src/index.ts
+++ apps/backend/src/index.ts
@@ -1,27 +1,6 @@
 import { createServer } from "./server";
 import { log } from "@repo/logger";
 
-// TODO: used automated tool like https://openapi-ts.dev/introduction
-export type Song = {
-  id: string;
-  name: string;
-  artist: string;
-};
-
-// TODO: use persistent storage like a database
-// In-memory storage
-let songs: Song[] = [];
-
-export function getSongsArray(): Song[] {
-  return songs;
-}
-
-// This is super unsafe!!
-// TODO: use proper service layer!
-export function setSongsArray(newSongs: Song[]) {
-  songs = newSongs;
-}
-
 const port = process.env.PORT || 3001;
 const server = createServer();
 
