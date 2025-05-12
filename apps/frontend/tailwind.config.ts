ade9f5fa06f23e36bf3e27f74dc5755c75fbfebb
Mate Szedlak <szedlakmate@gmail.com>
2025-05-12 02:18:31 +0100

    Configure DaisyUI on frontend

---
+++ apps/frontend/tailwind.config.ts
@@ -0,0 +1,17 @@
+import type { Config } from "tailwindcss";
+
+const config: Config = {
+  content: [
+    "./src/**/*.{js,ts,jsx,tsx}",
+    "../../packages/ui/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [require("daisyui")],
+  // @ts-expect-error: daisyui is injected by the plugin
+  daisyui: {
+    themes: ["light", "dark", "abyss"], // 'cupcake', 'bumblebee', etc.
+  },
+};
+export default config;
