add852b17ce8df05eb02409c167670e63db175d7
Mate Szedlak <szedlakmate@gmail.com>
2025-05-13 01:27:28 +0100

    Adjust cover image sizing

--- apps/frontend/src/app/components/SongCard.tsx
+++ apps/frontend/src/app/components/SongCard.tsx
@@ -12,7 +12,7 @@ export function SongCard(props: { song: Song; onDelete: () => void; onEdit: () =
       <div className="card card-side bg-base-200 relative h-32 w-full max-w-sm items-center shadow-xl md:h-48 md:max-w-2xl xl:h-64 xl:max-w-4xl">
         <SongActionsMenu onEdit={onEdit} onDelete={onDelete} />
 
-        <figure className="aspect-square w-24 md:w-32 xl:w-48">
+        <figure className="rounded-l-box h-full w-24 overflow-hidden md:w-32 xl:w-48">
           <Image
             src={sanitizeUrl(`${host}/${song.imageUrl}`)}
             alt={song.name}
@@ -23,7 +23,6 @@ export function SongCard(props: { song: Song; onDelete: () => void; onEdit: () =
           />
         </figure>
 
-        {/* Text */}
         <div className="card-body h-full justify-center px-4">
           <h2 className="card-title text-base md:text-lg xl:text-xl">{song.name}</h2>
           <p className="text-sm md:text-base">{song.artist}</p>
